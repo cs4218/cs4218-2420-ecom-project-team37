83e4b087b9e5956e23584ca59c42f74eb11b6e8e
Mok Yao Hui <yaohui788@gmail.com>
2025-02-26 13:10:30 +0800

    Update package json and backend test config

--- jest.backend.config.js
+++ jest.backend.config.js
@@ -8,8 +8,6 @@ export default {
   // which test to run
   testMatch: ["<rootDir>/controllers/*.test.js", "<rootDir>/middlewares/*.test.js", "<rootDir>/helpers/*.test.js", "<rootDir>/config/*.test.js"],
 
-  transform: {},
-
   // jest code coverage
   collectCoverage: true,
   collectCoverageFrom: ["controllers/**", "middlewares/**", "helpers/**", "config/**"],
