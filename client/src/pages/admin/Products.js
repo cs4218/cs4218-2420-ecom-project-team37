2d3de4453753fa9c3639efe3e47d8b85cbb1782f
JunHao247 <onlyjunhao247@gmail.com>
2025-03-08 16:34:13 +0800

    Update css to display cards properly

--- client/src/pages/admin/Products.js
+++ client/src/pages/admin/Products.js
@@ -4,33 +4,35 @@ import Layout from "./../../components/Layout";
 import axios from "axios";
 import toast from "react-hot-toast";
 import { Link } from "react-router-dom";
+import "../../styles/Products.css";
+
 const Products = () => {
   const [products, setProducts] = useState([]);
 
-  //getall products
+  // Get all products
   const getAllProducts = async () => {
     try {
       const { data } = await axios.get("/api/v1/product/get-product");
       setProducts(data.products);
     } catch (error) {
       console.log(error);
-      toast.error("Someething Went Wrong");
+      toast.error("Something Went Wrong");
     }
   };
 
-  //lifecycle method
   useEffect(() => {
     getAllProducts();
   }, []);
+
   return (
     <Layout>
-      <div className="row">
+      <div className="row product-page">
         <div className="col-md-3">
           <AdminMenu />
         </div>
-        <div className="col-md-9 ">
+        <div className="col-md-9">
           <h1 className="text-center">All Products List</h1>
-          <div className="d-flex">
+          <div className="d-flex flex-wrap gap-3">
             {products?.map((p) => (
               <Link
                 key={p._id}
