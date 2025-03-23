cb25eefe688fbaa2127ec86bcf901b5324c025d1
JunHao247 <onlyjunhao247@gmail.com>
2025-03-23 17:35:59 +0800

    Update UI testing

--- e2etests/adminCreateCategory.e2e.test.js
+++ e2etests/adminCreateCategory.e2e.test.js
@@ -64,3 +64,47 @@ test("Admin attempts to create a duplicate category", async ({ page }) => {
   await page.waitForSelector("text=Category already exists", { timeout: 5000 });
   await expect(page.locator("text=Category already exists")).toBeVisible();
 });
+
+test("Admin attempts to edit one category to another existing category", async ({ page }) => {
+  await page.goto("http://localhost:3000/login");
+  await page.getByPlaceholder("Enter Your Email ").fill("test-admin@example.com");
+  await page.getByPlaceholder("Enter Your Password").fill("test123");
+  await page.getByRole("button", { name: "LOGIN" }).click();
+
+  await page.waitForURL("http://localhost:3000");
+  await page.getByRole("button", { name: "Admin" }).click();
+  await page.getByRole("link", { name: "Dashboard" }).click();
+
+  await page.getByRole('link', { name: 'Create Category' }).click();
+  await page.waitForURL("http://localhost:3000/dashboard/admin/create-category");
+
+  await page.getByRole('button', { name: 'Edit' }).nth(1).click(); 
+    const dialog = page.locator('div[role="dialog"]');
+  await expect(dialog).toBeVisible();
+  
+  await dialog.getByPlaceholder("Enter new category").fill("TEST-Category One");
+  await dialog.getByRole("button", { name: "Submit" }).click();
+
+  await page.waitForSelector("text=Category with this name already exists", { timeout: 5000 });
+  await expect(page.locator("text=Category with this name already exists")).toBeVisible();
+});
+
+test("Admin attempts to delete a category", async ({ page }) => {
+  await page.goto("http://localhost:3000/login");
+  await page.getByPlaceholder("Enter Your Email ").fill("test-admin@example.com");
+  await page.getByPlaceholder("Enter Your Password").fill("test123");
+  await page.getByRole("button", { name: "LOGIN" }).click();
+
+  await page.waitForURL("http://localhost:3000");
+  await page.getByRole("button", { name: "Admin" }).click();
+  await page.getByRole("link", { name: "Dashboard" }).click();
+
+  await page.getByRole('link', { name: 'Create Category' }).click();
+  await page.waitForURL("http://localhost:3000/dashboard/admin/create-category");
+
+  await page.getByRole("button", { name: "Delete" }).nth(1).click();
+
+  await page.waitForSelector("text=category is deleted", { timeout: 5000 });
+  await expect(page.locator("text=category is deleted")).toBeVisible();
+});
+
